9185107d9b57d3a59ac703f75e6145166e13e06c
Justin Harwood <justin.harwood@benefitfocus.com>
2018-06-19 15:58:15 -0400

    InvertBinaryTree wip

--- medium/MaximumBinaryTree/index.js
+++ medium/MaximumBinaryTree/index.js
@@ -15,7 +15,7 @@ class Node {
   }
 }
 
-class Tree {
+class MBT {
   constructor(nums) {
     if (!nums.length) {
       return null
@@ -24,15 +24,15 @@ class Tree {
     let max = getMaxData(nums)
 
     const node = new Node(max.value)
-    node.left = new Tree(nums.slice(0, max.index))
-    node.right = new Tree(nums.slice(max.index + 1, nums.length))
+    node.left = new MBT(nums.slice(0, max.index))
+    node.right = new MBT(nums.slice(max.index + 1, nums.length))
 
     return node    
   }
 }
 
 const constructMaximumBinaryTree = nums => {
-  return new Tree(nums)
+  return new MBT(nums)
 }
 
 
